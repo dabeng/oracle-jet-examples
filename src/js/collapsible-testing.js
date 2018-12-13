76e319f0078ff147b6ecac9cc9fc0614e19a0f36
dabeng <dabeng413@gmail.com>
2018-12-13 15:08:40 +0800

    add new sample

---
+++ src/js/collapsible-testing.js
@@ -0,0 +1,52 @@
+'use strict';
+
+requirejs.config({
+  baseUrl: 'js',
+
+  // Path mappings for the logical module names
+  // Update the main-release-paths.json for release mode when updating the mappings
+  paths:
+  //injector:mainReleasePaths
+  {
+    'knockout': 'libs/knockout/knockout-3.4.2.debug',
+    'jquery': 'libs/jquery/jquery-3.3.1',
+    'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
+    'promise': 'libs/es6-promise/es6-promise',
+    'hammerjs': 'libs/hammer/hammer-2.0.8',
+    'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.0',
+    'ojs': 'libs/oj/v6.0.0/debug',
+    'ojL10n': 'libs/oj/v6.0.0/ojL10n',
+    'ojtranslations': 'libs/oj/v6.0.0/resources',
+    'text': 'libs/require/text',
+    'signals': 'libs/js-signals/signals',
+    'customElements': 'libs/webcomponents/custom-elements.min',
+    'proj4': 'libs/proj4js/dist/proj4-src',
+    'css': 'libs/require-css/css',
+    'touchr': 'libs/touchr/touchr'
+  }
+    //endinjector
+});
+
+require(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojcollapsible', 'ojs/ojbutton'],
+  function(oj, ko, $) { // this callback gets executed when all required modules are loaded
+
+    function ViewModel() {
+      var self = this;
+
+     self.expanded=ko.observable(false);
+     self.fontColor = 'none';
+     self.logoExpanded = ko.pureComputed(function() {
+       return self.expanded() ? 'none' : 'initial';
+     });
+      self.toggle = function (event) {
+        self.expanded(!self.expanded());
+        // $('#collapsiblePage')[0].expanded = !$('#collapsiblePage')[0].expanded;
+      }
+
+    }
+
+    $(function() {
+      ko.applyBindings(new ViewModel());
+    });
+
+});
