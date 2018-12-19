17e89215ae479703ad2d598f809a58ddcc66f2d8
dabeng <dabeng413@gmail.com>
2018-12-19 16:13:19 +0800

    WIP: add example

---
+++ src/js/dynamic-accordion.js
@@ -0,0 +1,48 @@
+'use strict';
+
+requirejs.config({
+  baseUrl: 'js',
+
+  // Path mappings for the logical module names
+  // Update the main-release-paths.json for release mode when updating the mappings
+  paths:
+  //injector:mainReleasePaths
+  {
+    'knockout': 'libs/knockout/knockout-3.4.2.debug',
+    'jquery': 'libs/jquery/jquery-3.3.1',
+    'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
+    'promise': 'libs/es6-promise/es6-promise',
+    'hammerjs': 'libs/hammer/hammer-2.0.8',
+    'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.0',
+    'ojs': 'libs/oj/v6.0.0/debug',
+    'ojL10n': 'libs/oj/v6.0.0/ojL10n',
+    'ojtranslations': 'libs/oj/v6.0.0/resources',
+    'text': 'libs/require/text',
+    'signals': 'libs/js-signals/signals',
+    'customElements': 'libs/webcomponents/custom-elements.min',
+    'proj4': 'libs/proj4js/dist/proj4-src',
+    'css': 'libs/require-css/css',
+    'touchr': 'libs/touchr/touchr'
+  }
+    //endinjector
+});
+
+require(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojaccordion'],
+  function(oj, ko, $) {
+
+    function ViewModel() {
+      var self = this;
+
+      self.panels = ko.observableArray([
+        { 'heading': 'Head 1', 'expaned': false },
+        { 'heading': 'Head 2', 'expaned': false },
+        { 'heading': 'Head 3', 'expaned': false }
+      ]);
+
+    }
+
+    $(function() {
+      ko.applyBindings(new ViewModel());
+    });
+
+});
