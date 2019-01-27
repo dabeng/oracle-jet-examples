777fa9575e256edc78cb5dd7420a21ea2eb4c5be
dabeng <dabeng413@gmail.com>
2019-01-27 23:25:50 +0800

    WIP: crud list

--- src/js/listview-crud.js
+++ src/js/listview-crud.js
@@ -29,112 +29,165 @@ requirejs.config({
 
 require(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojarraydataprovider', 'ojs/ojknockout', 'ojs/ojlistview', 'ojs/ojmodel',
   'ojs/ojcollectiontabledatasource', 'ojs/ojinputtext', 'ojs/ojinputnumber', 'ojs/ojdatetimepicker', 'ojs/ojselectcombobox'],
-function(oj, ko, $, ArrayDataProvider)
-{
-    function viewModel()
-    {
-      var self = this;
+function (oj, ko, $, ArrayDataProvider) {
+  function ViewModel () {
+    var self = this;
 
-      self.dateConverter = oj.Validation.converterFactory('datetime').createConverter({ formatStyle: 'date', dateFormat: 'medium' });
-      self.formatDate = function (dateStr) {
-        return self.dateConverter.format(oj.IntlConverterUtils.dateToLocalIso(new Date(dateStr)));
-      };
-      self.salaryConverter = oj.Validation.converterFactory('number').createConverter({ style: 'currency', currency: 'USD' });
-      self.formatSalary = function (numberStr) {
-        return self.salaryConverter.format(numberStr);
-      };   
-      self.getDeptName = function (deptId) {
-        var deptName = '';
-        switch (deptId) {
-          case 10:
-            deptName = 'Administration';
-            break;
-          case 20:
-            deptName = 'Marketing';
-            break;
-          case 30:
-            deptName = 'Sales';
-            break;
-          case 40:
-            deptName = 'Finance';
-            break;
-          case 50:
-            deptName = 'Human Resources';
-            break;
-        }
-        return deptName;
-      };
+    self.dateConverter = oj.Validation.converterFactory('datetime').createConverter({ formatStyle: 'date', dateFormat: 'medium' });
+    self.formatDate = function (dateStr) {
+      return self.dateConverter.format(oj.IntlConverterUtils.dateToLocalIso(new Date(dateStr)));
+    };
+    self.salaryConverter = oj.Validation.converterFactory('number').createConverter({ style: 'currency', currency: 'USD' });
+    self.formatSalary = function (numberStr) {
+      return self.salaryConverter.format(numberStr);
+    };   
+    self.getDeptName = function (deptId) {
+      var deptName = '';
+      switch (deptId) {
+        case 10:
+          deptName = 'Administration';
+          break;
+        case 20:
+          deptName = 'Marketing';
+          break;
+        case 30:
+          deptName = 'Sales';
+          break;
+        case 40:
+          deptName = 'Finance';
+          break;
+        case 50:
+          deptName = 'Human Resources';
+          break;
+      }
+      return deptName;
+    };
 
-      self.collection = new oj.Collection(null, {
-        model: new oj.Model.extend({idAttribute: 'EMPLOYEE_ID'}),
-        fetchSize: 5,
+    self.modelAjax = function (operation, model, options) {
+      var request = { url: 'http://localhost:3000/employees' };
+      if (operation === 'update' || operation === 'delete') {
+        request.url = request.url + '/' + options.recordID;
+      }
+      return request;
+    };
+
+    self.collectionAjax = function (operation, collection, options) {
+      var request = {
         url: 'http://localhost:3000/employees',
-        customURL: function () {
-          return {
-            // url: 'http://localhost:3000/employees',
-            data: {
-              _start: self.collection.length,
-              _limit: 5
-            }
-          }
+        data: {
+          _start: self.collection.length,
+          _limit: 5
         }
-      });        
-      self.dataSource = ko.observable(new oj.CollectionTableDataSource(self.collection));
-
-      self.collection.on('request', function() {
-        console.log('request more data');
-      });
-
-      self.collection.on('sync', function() {
-        console.log('sync');
-      });
-
-      self.collection.on('ready', function() {
-        console.log('ready');
-      });
-
-      self.handleSelectionChanged = function (event) {
-        var a = event.detail.value;
       };
+      if (self.currentSort() !== 'default') {
+        request.data._sort = self.sortCriteria[self.currentSort()].key;
+        request.data._order = self.sortCriteria[self.currentSort()].direction;
+      }
+      if (self.currentFilter() !== 'all') {
+        request.data.DEPARTMENT_ID = self.currentFilter();
+      }
+      if (self.currentNameSearch().trim()) {
+        request.data.NAME_like = self.currentNameSearch().trim();
+      }
+      if (self.currentGlobalSearch().trim()) {
+        request.data.q = self.currentGlobalSearch().trim();
+      }
+      return request;
+    };
 
-    self.name = ko.observable('');
-    self.hireDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date()));
-    this.department = ko.observable(30);
-    this.deptOptions = new ArrayDataProvider([
-      {value: 10, label: 'Administration'},
-      {value: 20, label: 'Marketing'},
-      {value: 30, label: 'Sales'},
-      {value: 40, label: 'Finance'},
-      {value: 50, label: 'Human Resources'}
-    ], { keyAttributes: 'value' });
-    self.salary = ko.observable(0);
+    self.collection = new oj.Collection(null, {
+      model: oj.Model.extend({
+        idAttribute: 'id',
+        customURL: self.modelAjax
+      }),
+      fetchSize: 5,
+      customURL: self.collectionAjax
+    });   
+    self.dataSource = ko.observable(new oj.CollectionTableDataSource(self.collection));
+
+    self.collection.on('request', function() {
+      console.log('request more data');
+    });
+
+    self.collection.on('sync', function() {
+      console.log('sync');
+    });
+
+    self.collection.on('ready', function() {
+      console.log('ready');
+    });
+
+    self.fillOutform = function (id) {
+      self.collection.get(id).then(function (model) {
+        self.modelToUpdate = model;
+        self.inputId(model.get('id'));
+        self.inputName(model.get('NAME'));
+        self.inputHireDate(oj.IntlConverterUtils.dateToLocalIso(new Date(model.get('HIRE_DATE'))));
+        self.inputSalary(model.get('SALARY'));
+        self.inputDepartment(model.get('DEPARTMENT_ID'));
+      });
+    };
+
+    // reset the fields to their original values
+    self.resetFields = function () {
+      self.inputId(self.uuidv4());
+      self.inputName('');
+      self.inputHireDate(oj.IntlConverterUtils.dateToLocalIso(new Date()));
+      self.inputDepartment(30);
+      self.inputSalary(0);
+    }; 
+
+    // update the fields based on the selected item
+    self.handleSelectionChanged = function (event) {
+      if (event.detail.value[0]) { // select one item
+        self.fillOutform(event.detail.value[0]);
+      } else { // current selected item is removed
+        self.resetFields();
+      }
+    };
 
     self.uuidv4 = function () {
       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
       });
-    }
+    };
+    
 
+    // intialize the observable values in the form
+    self.inputId = ko.observable(self.uuidv4());
+    self.inputName = ko.observable('');
+    self.inputHireDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date()));
+    self.inputDepartment = ko.observable(30);
+    self.deptOptions = new ArrayDataProvider([
+      {value: 10, label: 'Administration'},
+      {value: 20, label: 'Marketing'},
+      {value: 30, label: 'Sales'},
+      {value: 40, label: 'Finance'},
+      {value: 50, label: 'Human Resources'}
+    ], { keyAttributes: 'value' });
+    self.inputSalary = ko.observable(0);
 
-    //build a new model from the observables in the form
+    // build a new model from the observables in the form
     self.buildModel = function () {
       return {
-        'id': self.uuidv4(),
-        'NAME': self.name(),
-        'HIRE_DATE': self.hireDate(),
-        'SALARY': self.salary(),
-        'DEPARTMENT_ID': self.department()
+        'id': self.inputId(),
+        'NAME': self.inputName(),
+        'HIRE_DATE': self.inputHireDate(),
+        'SALARY': self.inputSalary(),
+        'DEPARTMENT_ID': self.inputDepartment()
       };
     };
 
-    //add the model to the collection 
+    // add the model to the collection 
     self.add = function () {
-      var model = self.buildModel();
-      self.collection.create(model, {
+      self.inputId(self.uuidv4());
+      self.collection.create(self.buildModel(), {
         wait:true,
-        at:0,
+        // merge: true,
+        silent: true,
         success: function (model, response, options) {
+          self.collection.refresh();
           console.log('Create operation succeeds');
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -146,145 +199,68 @@ function(oj, ko, $, ArrayDataProvider)
       });
     };
 
+    // update the model in the collection
     self.update = function () {
-
-    };
-
-    self.remove = function () {
-
-    }
-      // self.collection = null;
-      // self.dataSource = ko.observable();
-      // $.getJSON('js/employeeData.json',function (data) {
-      //   var server = new MockPagingRESTServer({ 'Employees': data}, { collProp: 'Employees', id: 'EMPLOYEE_ID', responseTime: 1000});
-      //   self.collection = new oj.Collection(null, {
-      //     url: server.getURL(),
-      //     fetchSize: 5,
-      //     model: new oj.Model.extend({idAttribute: 'EMPLOYEE_ID'}),
-      //   });
-      //   self.dataSource(new oj.CollectionTableDataSource(self.collection));
-      // });
-
-      var sortCriteria = {
-        'default': { 'key': 'EMPLOYEE_ID', 'direction': 'ascending' },
-        'hdAsc': { 'key': 'HIRE_DATE', 'direction': 'ascending' },
-        'hdDesc': { 'key': 'HIRE_DATE', 'direction': 'descending' },
-        'salAsc': { 'key': 'SALARY', 'direction': 'ascending' },
-        'salDesc': { 'key': 'SALARY', 'direction': 'descending' }
-      };
-      self.currentSort = ko.observable('default');
-      self.sortList = function () {
-        self.dataSource().sort(sortCriteria[self.currentSort()]);
-      };
-
-      self.currentFilter = ko.observable('');
-      var originalCollection = self.collection;
-      function filterFunc (model) {
-        return model.get('DEPARTMENT_ID') === parseInt(self.currentFilter());
+      if (self.modelToUpdate) {
+        self.modelToUpdate.save(self.buildModel(), {
+          success: function (model, response, options) {
+            console.log('Update operation succeeds');
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log('Error in Update: ' + textStatus);
+          }
+        });
       }
-      self.filterList = function(event, ui) {      
-        if (self.currentFilter() === 'all') {
-          self.collection = originalCollection;
-        } else {
-          self.collection = new oj.Collection(originalCollection.filter(filterFunc));
-        }
-        self.dataSource(new oj.CollectionTableDataSource(self.collection));
-
-        if (self.currentSort() !== 'default') {
-          self.dataSource().sort(sortCriteria[self.currentSort()]);
-        }
-      };
-
-      self.currentSearch = ko.observable('');
-      self.nameFilter = function (model, attr, value) {
-        return model.get('NAME').toLowerCase().indexOf(value.toLowerCase()) > -1;
-      };
-      self.searchList = function(event, ui) {
-        self.dataSource(new oj.CollectionTableDataSource(self.collection.whereToCollection({ 'NAME':{ 'value': self.currentSearch(), 'comparator': self.nameFilter }})));
-      };
-
-      // var nextKey = 121;        
-
-      // //build a new model from the observables in the form
-      // self.buildModel = function() {
-      //   return {
-      //     'EMPLOYEE_ID': self.inputEmployeeID(),
-      //     'FIRST_NAME': self.inputFirstName(),
-      //     'LAST_NAME': self.inputLastName(),
-      //     'HIRE_DATE': self.inputHireDate(),
-      //     'SALARY': self.inputSalary()
-      //   };
-      // };
-        
-      // //used to update the fields based on the selected row
-      // self.updateFields = function(model) {      
-      //   self.inputEmployeeID(model.get('EMPLOYEE_ID'));
-      //   self.inputFirstName(model.get('FIRST_NAME'));
-      //   self.inputLastName(model.get('LAST_NAME'));
-      //   self.inputHireDate(model.get('HIRE_DATE'));
-      //   self.inputSalary(model.get('SALARY'));
-      // };
-
-      // //add the model to the collection at index 0
-      // self.add = function() {
-      //   if (self.inputEmployeeID(nextKey) < nextKey) {
-      //     self.inputEmployeeID(nextKey);            
-      //   }
-      //   var model = self.buildModel();
-      //   nextKey += 1;
-      //   self.inputEmployeeID(nextKey);
-      //   self.collection.add(model, {at:0});
-      // };
-
-      // // update the model in the collection
-      // self.update = function() {
-      //   if (self.modelToUpdate) {
-      //     self.modelToUpdate.set(self.buildModel());
-      //   }
-      // };
-
-      // //remove the selected model from the collection
-      // self.remove = function() {
-      //   self.collection.remove(self.modelToUpdate);
-      // };
-
-      // //reset the fields to their original values
-      // self.resetFields = function() {       
-      //   self.inputEmployeeID(nextKey);
-      //   self.inputFirstName('Jane');
-      //   self.inputLastName('Doe');
-      //   self.inputHireDate(oj.IntlConverterUtils.dateToLocalIso(new Date()));
-      //   self.inputSalary(15000);
-      // };  
-
-      // //intialize the observable values in the forms
-      // self.inputEmployeeID = ko.observable(nextKey);
-      // self.inputFirstName = ko.observable('Jane');
-      // self.inputLastName = ko.observable('Doe');
-      // self.inputHireDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date()));
-      // self.inputSalary = ko.observable(15000);
-
-      // self.getCellClassName = function(cellContext) {
-      //   var key = cellContext['keys']['column'];
-      //   if (key === 'SALARY') { 
-      //     return 'oj-helper-justify-content-right';
-      //   }
-      //   return 'oj-helper-justify-content-flex-start';                          
-      // }
-
-      // document.getElementById('datagrid').addEventListener('selectionChanged', function(event) {
-      //   //on selection change update fields with the selected model
-      //   var selection = event.detail['value'][0];
-      //   if (selection != null) {
-      //     var rowKey = selection['startKey']['row'];
-      //     self.modelToUpdate = self.collection.get(rowKey);
-      //     self.updateFields(self.modelToUpdate);
-      //   }
-      // });        
     };
 
-    $(function() {
-      var vm = new viewModel();
-      ko.applyBindings(vm, document.getElementById('datagrid-wrapper'));
-    });
+    // remove the selected model from the collection
+    self.remove = function () {
+      if (self.modelToUpdate) {
+        self.modelToUpdate.destroy({
+          wait: true,
+          success: function (model, response, options) {
+            console.log('Delete operation succeeds');
+          },
+          error: function (model, xhr, status, error) {
+            console.log('Error in Delete: ' + textStatus);
+          }
+        });
+      }
+    };
+
+    // sort the list according to the HIRE_DATE or SALARY
+    self.sortCriteria = {
+      'default': { 'key': 'id', 'direction': 'asc' },
+      'hdAsc': { 'key': 'HIRE_DATE', 'direction': 'asc' },
+      'hdDesc': { 'key': 'HIRE_DATE', 'direction': 'desc' },
+      'salAsc': { 'key': 'SALARY', 'direction': 'asc' },
+      'salDesc': { 'key': 'SALARY', 'direction': 'desc' }
+    };
+    self.currentSort = ko.observable('default');
+    self.sortList = function () {
+      self.collection.comparator = '';
+      self.collection.sort();
+    };
+
+    self.currentFilter = ko.observable('all');
+    self.filterList = function (event, ui) {
+      self.collection.refresh();
+    };
+
+    self.currentNameSearch = ko.observable('');
+    self.searchList = function(event, ui) {
+      self.collection.refresh();
+    };
+
+    self.currentGlobalSearch = ko.observable('');
+    self.globalSearchList = function(event, ui) {
+      self.collection.refresh();
+    };
+
+  };
+
+  $(function() {
+    var vm = new ViewModel();
+    ko.applyBindings(vm, document.getElementById('datagrid-wrapper'));
+  });
 });
